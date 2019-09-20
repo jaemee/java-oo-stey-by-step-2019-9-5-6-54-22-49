b63ee5dd56010acc164a5ee2c10dd2ef24877df3
bugarja <jae.melanee.bugarin@oocl.com>
2019-09-20 23:27:29 +0800

    modified the classes under package Practice08, added validation to check the student under a certain teacher modify the equals method to check the equality of the object

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student student;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    void assignLeader(Student student){
+        this.student = student;
+    }
+
+    Student getLeader() {
+        return this.student;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,54 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+    private int id;
+    private int age;
+    private String name;
+
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        StringBuilder intro = new StringBuilder(" I am a Teacher. I teach ");
+        if(getKlass()!= null){
+            intro.append(getKlass().getDisplayName()).append(".");
+        }else {
+            intro.append("No Class.");
+        }
+
+        return super.introduce().concat(intro.toString());
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    String introduceWith(Student student){
+        StringBuilder introWith = new StringBuilder(super.introduce()).append(" I am a Teacher.");
+        if(getKlass().equals(student.getKlass())){
+            introWith.append(" I teach ").append(student.getName()).append(".");
+        }else {
+            introWith.append(" I don't teach ").append(student.getName()).append(".");
+        }
+        return introWith.toString();
+    }
+
 }
