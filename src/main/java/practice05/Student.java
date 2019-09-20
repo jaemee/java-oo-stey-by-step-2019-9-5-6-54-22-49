d9f37d04831f0f3e26c9772f39a3d67c898498bf
bugarja <jae.melanee.bugarin@oocl.com>
2019-09-20 18:56:56 +0800

    modify class Person for Practice5, Use inheritance for Student and Worker and use overriden method to modify the returned value with calling superClass method values to pass the test for Practice 5

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,27 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        StringBuilder intro = new StringBuilder(" I am a Student. I am at Class ").append(getKlass()).append(".");
+        return super.introduce().concat(intro.toString());
+    }
 }
