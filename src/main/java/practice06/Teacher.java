b9e35ad848f3cb121f1a50fecac26cffe429e46d
bugarja <jae.melanee.bugarin@oocl.com>
2019-09-20 19:10:10 +0800

    modify class Person for Practice6, override methods from superclass and add validation for different value returns to pass the test for Practice 6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        StringBuilder intro = new StringBuilder(" I am a Teacher. I teach ");
+        if(getKlass()!= null){
+            intro.append("Class ").
+                    append(getKlass()).append(".");
+        }else {
+            intro.append("No Class.");
+        }
+
+        return super.introduce().concat(intro.toString());
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
 }
