6f1d3ba98d10414118e29210a193147c256e5daf
bugarja <jae.melanee.bugarin@oocl.com>
2019-09-22 14:00:00 +0800

    modify classes to pass the test in Practice11, make Klass notify Teacher by making the 2 classes Observer and Observable and take use of Observer Pattern as per Requirement

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,45 @@
 package practice11;
 
-public class Klass {
+import java.util.Observable;
+
+public class Klass extends Observable {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    void assignLeader(Student student) {
+        if(getNumber()!=student.getKlass().getNumber()){
+            System.out.append("It is not one of us.\n");
+        }else{
+            this.leader = student;
+            System.out.append("I am Tom. I know ").append(student.getName())
+                    .append(" become Leader of ").append(getDisplayName()).append(".\n");
+            setChanged();
+            notifyObservers(getNumber());
+        }
+    }
+
+    Student getLeader() {
+        return this.leader;
+    }
+
+    void appendMember(Student student) {
+            student.getKlass().number = this.getNumber();
+            System.out.append("I am Tom. I know ").append(student.getName())
+                    .append(" has joined ").append(getDisplayName()).append(".\n");
+            setChanged();
+            notifyObservers(getNumber());
+
+    }
 }
--- src/main/java/practice11/Person.java
+++ src/main/java/practice11/Person.java
@@ -1,4 +1,46 @@
 package practice11;
 
 public class Person {
+    private Integer age;
+    private String name;
+    private Integer id;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(Integer id, String name, Integer age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public Person() {
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String introduce(){
+        StringBuilder intro = new StringBuilder("My name is ").append(getName()).append(". I am ").append(getAge()).append(" years old.");
+        return intro.toString();
+    }
+
+    public boolean equals(Object second){
+        if(!(second instanceof Object))
+            return false;
+        Person temp = (Person) second;
+        return this.name.equals(temp.name) && this.id.equals(temp.id) && this.age.equals(temp.age);
+    }
+
 }
